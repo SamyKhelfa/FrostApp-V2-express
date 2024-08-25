b4a04f5cd06e784bbc9572ca8de7ed81fec39a2d
samy <samy@gmail.com>
2024-08-25 12:43:02 +0200

    update server

--- server.ts
+++ server.ts
@@ -1,14 +1,15 @@
-const express = require('express');
+import express from 'express';
+import { router as registerRouter } from './routes/register';
+import { router as loginRouter } from './routes/login';
+
 const app = express();
 const port = 3000;
-import './routes/index';
-import './routes/register';
-import './routes/login';
-import './routes/user';
 
+app.use(express.json());  // Pour parser le JSON dans le corps des requêtes
+
+app.use('/auth', registerRouter);
+app.use('/auth', loginRouter);
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
--- utils/errors.ts
+++
@@ -1,6 +0,0 @@
-export class UserInputError extends Error {
-    constructor(message: string) {
-      super(message);
-      this.name = 'UserInputError';
-    }
-  }
